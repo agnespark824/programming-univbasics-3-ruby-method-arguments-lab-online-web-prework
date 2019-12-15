f1f8a86d252b63989ff223883f3b8d3d82d10388
Agnes Park <agnespark824@gmail.com>
2019-12-15 21:59:48 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,10 +6,8 @@ def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional (name, language)
-  if language == null
-  puts "Hi, my name is #{name} and I am learning to program in Ruby."
-  else "Hi, my name is #{name} and I am learning to program in #{language}." 
+def introduction_with_language_optional (name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 
